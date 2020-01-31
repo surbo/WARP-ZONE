8caa9b853922eb6c49c0bd4f7049592917d683ad
SuRb0 <1809870+surbo@users.noreply.github.com>
2020-01-31 07:53:49 -0600

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,14 +1,20 @@
 cls
 echo "I"
+Start-Sleep -s 2
 cls
 echo "I DID"
+Start-Sleep -s 2
 cls
 echo "I DID THIS"
+Start-Sleep -s 2
 cls
 echo "I DID THIS AS"
+Start-Sleep -s 2
 cls
 echo "I DID THIS AS A"
+Start-Sleep -s 2
 cls
 echo "I DID THIS AS A TEST"
+Start-Sleep -s 2
 cls
 echo "Thank you for subscribing to Cat Facts!"
